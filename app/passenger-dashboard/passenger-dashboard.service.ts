d6499625a23ef37a0b764760db02dd75592e17cf
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2017-08-27 17:51:30 -0500

    Add catch

--- app/passenger-dashboard/passenger-dashboard.service.ts
+++ app/passenger-dashboard/passenger-dashboard.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
 
 import { Passenger } from './models/passenger.interface';
 
@@ -9,23 +11,26 @@ const passengerApi = '/api/passengers';
 
 @Injectable()
 export class PassengerDashboardService {
-  constructor(private http: Http) {}
+  constructor(private http: Http) { }
 
   getPassengers(): Observable<Passenger[]> {
     return this.http
       .get(passengerApi)
-      .map(res => res.json());
+      .map(res => res.json())
+      .catch(err => Observable.throw(err.json()))
   }
 
   updatePassenger(passenger: Passenger): Observable<Passenger> {
     return this.http
       .put(`${passengerApi}/${passenger.id}`, passenger)
-      .map(res => res.json());
+      .map(res => res.json())
+      .catch(err => Observable.throw(err.json()))
   }
 
   removePassenger(passenger: Passenger): Observable<Passenger> {
     return this.http
       .delete(`${passengerApi}/${passenger.id}`)
-      .map(res => res.json());
+      .map(res => res.json())
+      .catch(err => Observable.throw(err.json()))
   }
 }
