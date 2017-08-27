1e5f629458a3279a0091835a757af8216ead6b2b
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2017-08-27 16:48:38 -0500

    Add Output events

--- app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
+++ app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Passenger } from '../../models/passenger.interface';
 
 @Component({
@@ -8,6 +8,25 @@ import { Passenger } from '../../models/passenger.interface';
 })
 export class PassengerDetailComponent {
   @Input() detail: Passenger;
+  @Output() remove: EventEmitter<Passenger> = new EventEmitter();
+  @Output() edit: EventEmitter<Passenger> = new EventEmitter();
+
+  editing: boolean = false;
 
   constructor() { }
+
+  onNameChange(value: string): void {
+    this.detail.fullname = value;
+  }
+
+  toggleEdit(): void {
+    if (this.editing) {
+      this.edit.emit(this.detail);
+    }
+    this.editing = !this.editing;
+  }
+
+  onRemove(): void {
+    this.remove.emit(this.detail);
+  }
 }
