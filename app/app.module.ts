7701e28443c5774645667b2d5d0c0a6aee3da56a
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2017-08-27 15:25:15 -0500

    Create passenger dashboard module

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
+import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    PassengerDashboardModule
   ],
   bootstrap: [
     AppComponent
