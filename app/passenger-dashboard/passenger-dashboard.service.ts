b6083e15047740480af586f0c1a4bb9d8e00959a
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2017-08-27 17:30:33 -0500

    Use http

--- app/passenger-dashboard/passenger-dashboard.service.ts
+++ app/passenger-dashboard/passenger-dashboard.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 import { Passenger } from './models/passenger.interface';
+
+const passengerApi = '/api/passengers';
 
 @Injectable()
 export class PassengerDashboardService {
   constructor(private http: Http) {}
 
-  getPassengers(): Passenger[] {
-    return ;
+  getPassengers(): Observable<Passenger[]> {
+    return this.http.get(passengerApi)
+      .map(res => res.json());
   }
 }
