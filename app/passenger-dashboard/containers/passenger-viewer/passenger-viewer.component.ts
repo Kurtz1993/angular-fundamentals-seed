98ee4e65a87e5b95a2875e8dc79d16d058c36e83
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2017-08-27 20:50:16 -0500

    Scaffold component

---
+++ app/passenger-dashboard/containers/passenger-viewer/passenger-viewer.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'passenger-viewer',
+  styleUrls: ['./passenger-viewer.component.scss'],
+  templateUrl: './passenger-viewer.component.html'
+})
+export class PassengerViewerComponent {}
