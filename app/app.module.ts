6d87f5321c20941c95370c0e66542f585e980103
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2017-08-27 22:07:58 -0500

    Add router

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,21 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes, Route } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home.component';
+import { NotFoundComponent } from './not-found.component';
 import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
 
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: '*', component: NotFoundComponent }
+];
+
 @NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent
+  ],
   imports: [
     BrowserModule,
     CommonModule,
+    RouterModule.forRoot(routes),
     PassengerDashboardModule
   ],
   bootstrap: [
     AppComponent
-  ],
-  declarations: [
-    AppComponent
   ]
 })
-export class AppModule {}
+export class AppModule { }
