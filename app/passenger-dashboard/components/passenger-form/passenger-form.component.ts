7601bee5b48b6e5a053a4d986f8c0f83ca04cb79
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2017-08-27 21:06:18 -0500

    Add template and styles

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -3,7 +3,9 @@ import { Component, Input } from '@angular/core';
 import { Passenger } from '../../models/passenger.interface';
 
 @Component({
-  selector: 'passenger-form'
+  selector: 'passenger-form',
+  styleUrls: ['./passenger-form.component.scss'],
+  templateUrl: './passenger-form.component.html'
 })
 export class PassengerFormComponent {
   @Input() detail: Passenger;
